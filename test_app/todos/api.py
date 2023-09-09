eb025a31bd1e7e68a4fa92819870d7d07e922807
Savitha Sundaresan <savisrisundar@gmail.com>
2023-09-09 16:45:29 +0530

    completed the whole project

--- test_app/todos/api.py
+++ test_app/todos/api.py
@@ -1,8 +1,8 @@
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from test_app.core.db import get_async_session
 from test_app.todos import crud as todos_crud
-from test_app.todos.schemas import todosSchema
+from test_app.todos.schemas import todosSchema,todosSchemaCreate
 router=APIRouter()
 @router.get("/",response_model=list[todosSchema],status_code=200,)
 
@@ -12,8 +12,17 @@ async def get_todos(db=Depends(get_async_session)):
 
 @router.post("/",response_model=todosSchema,status_code=201,)
 
-async def create_todos(todos:todosSchema,db=Depends(get_async_session)):
-    todos=await todos_crud.create_todos(db)
+async def create_todos(todos:todosSchemaCreate,db=Depends(get_async_session)):
+    todos=await todos_crud.create_todos(db,todos)
+    return todos
+
+@router.put("/{id}",response_model=todosSchema,status_code=202)
+async def update_todos(id:int,todos:todosSchema,db=Depends(get_async_session)):
+    todos=await todos_crud.update_todos(id,todos,db)
     return todos
     
+@router.delete("/{id}",status_code=200)
 
+async def delete_todos(id:int,db=Depends(get_async_session)):
+    todo=await todos_crud.delete_todos(id,db)
+    return todo
