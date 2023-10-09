6cd7fe0c0030c85efc78ed4b2aaeca1497b6cc23
Savitha Sundaresan <savisrisundar@gmail.com>
2023-10-09 17:54:05 +0530

    3

--- test_app/core/config.py
+++ test_app/core/config.py
@@ -3,13 +3,13 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     
     #base connection
-    api_v1_prefix:str
-    debug:bool
-    project_name:str
-    version:str
-    description:str
-    db_async_connection_str:str
-    db_async_connection_str:str
+    api_v1_prefix:str="/api/v1"
+    debug:bool=True
+    project_name:str="FASTAPI APP (local)"
+    version:str="0.1.0"
+    description:str="The API for FASTAPI app."
+    db_async_connection_str:str="postgresql+asyncpg://postgres:Srihari1!@localhost:5432/Test_fastapi"
+    db_sync_connection_str:str="postgresql+pg8000://postgres:Srihari1!@localhost:5432/Test_fastapi"
     
     
     
