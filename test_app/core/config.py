7fa9988f251aa531c477bad7d341a60dc2d816bb
Savitha Sundaresan <savisrisundar@gmail.com>
2023-10-10 23:33:41 +0530

    200

--- test_app/core/config.py
+++ test_app/core/config.py
@@ -8,8 +8,8 @@ class Settings(BaseSettings):
     project_name:str="FASTAPI APP (local)"
     version:str="0.1.0"
     description:str="The API for FASTAPI app."
-    db_async_connection_str:str="postgresql+asyncpg://postgres:Srihari1!@render.com:5432/Test_fastapi"
-    db_sync_connection_str:str="postgresql+pg8000://postgres:Srihari1!@render.com:5432/Test_fastapi"
+    db_async_connection_str:str="postgresql+asyncpg://postgres:Srihari1!@localhost:5432/Test_fastapi"
+    db_sync_connection_str:str="postgresql+pg8000://postgres:Srihari1!@localhost:5432/Test_fastapi"
     
     
     
